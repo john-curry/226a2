f0aa2f387836c53605f0955e40187b857681a713
john-curry <liquidsnakeeater@gmail.com>
2016-02-05 09:39:05 -0800

    finished

--- MinMax.java
+++ MinMax.java
@@ -1,23 +1,9 @@
 import java.util.*;
 import java.lang.Math;
 class MinMax {
-	private static class Pair {
-		int alpha;   // the smaller one 
-		int omega; // the bigger one 
-		Pair ( int a, int o ) { alpha = a; omega = o; }
-    public String toString() {
-      return "("+this.alpha+","+this.omega+")";
-    }
-    public boolean equals(Object o) {
-      if (!(o instanceof Pair)) return false;
-      Pair p = (Pair)o;
-      return (p.omega == this.omega) && (p.alpha == this.alpha);
-    }
-	}
   public static int cmp = 0;	
 
   public static Pair mmThree(int a, int b, int c) {
-    System.out.println("Comparing 3 values: " + a + " " + b + " " + c);
     cmp++;
     if (a < b) {
       cmp++;
@@ -52,7 +38,6 @@ class MinMax {
       return new Pair(a[ub], a[ub]);
     }
 		if (ub - lb == 1) {
-      System.out.println("Upper bound: " + ub + " lower bound: " + lb);
       cmp++;
 			if (a[ub] < a[lb]) {
 				return new Pair(a[ub], a[lb]);
@@ -68,12 +53,9 @@ class MinMax {
       } else {
         pivot = (split + 1) / 2;
       }
-      System.out.println("Lower Pair upper bound: " + ub + " lower bound: " + lb + " pivot: " + pivot);
       Pair lbp = mmA(lb, pivot - 1 + lb, a);
-      System.out.println("Upper Pair upper bound: " + ub + " lower bound: " + (lb + pivot) + " pivot: " + pivot);
       Pair ubp = mmA(lb + pivot, ub, a);
 
-      System.out.println("Subpairs: " + lbp.toString() + ubp.toString());
 
       cmp++;
 			int lower = lbp.alpha < ubp.alpha ? lbp.alpha : ubp.alpha;
@@ -83,7 +65,6 @@ class MinMax {
       assert lower <= upper : "Lower is greater than upper";
 
       Pair p = new Pair(lower, upper);
-      System.out.println("Better pair: " + p.toString());
       return p;
 		}
 		
@@ -95,7 +76,6 @@ class MinMax {
       return new Pair(a[ub], a[ub]);
     }
 		else if (ub - lb == 1) {
-      System.out.println("Upper bound: " + ub + " lower bound: " + lb);
       cmp++;
 			if (a[ub] < a[lb]) {
 				return new Pair(a[ub], a[lb]);
@@ -105,7 +85,6 @@ class MinMax {
 		}
     else if (ub - lb == 2) {
       Pair t = mmThree(a[lb], a[lb+1], a[lb+2]);
-      System.out.println("Three pair compare: " + t.toString());
       return t;
     } 
       
@@ -117,12 +96,9 @@ class MinMax {
       } else {
         pivot = (split + 1) / 2;
       }
-      System.out.println("Lower Pair upper bound: " + ub + " lower bound: " + lb + " pivot: " + pivot);
       Pair lbp = mmB(lb, pivot - 1 + lb, a);
-      System.out.println("Upper Pair upper bound: " + ub + " lower bound: " + (lb + pivot) + " pivot: " + pivot);
       Pair ubp = mmB(lb + pivot, ub, a);
 
-      System.out.println("Subpairs: " + lbp.toString() + ubp.toString());
 
       cmp++;
 			int lower = lbp.alpha < ubp.alpha ? lbp.alpha : ubp.alpha;
@@ -132,30 +108,12 @@ class MinMax {
       assert lower <= upper : "Lower is greater than upper";
 
       Pair p = new Pair(lower, upper);
-      System.out.println("Better pair: " + p.toString());
       return p;
 		}
 	}
 	
 	public static void main(String[] args) {
-		int[] a = { 10, 4, 0, 5, 265, 1000, 1, 2, 4, 8, 50, 32 , 100, 43, 54, 103, 5, 2, 1 , 99, 65, 432, 1, 99, 43};
-    System.out.println(Arrays.toString(a));
-	  int n = a.length;
-    Pair exp = new Pair(0, 1000);
-
-    double optimal = ((double)(3 * n) / 2.0) - 2.0;
-		Pair one = mmA(0, a.length - 1, a);
-    
-    System.out.println("Found min/max mmA in " + cmp + " compares. Should have gotten: " + optimal);
-    System.out.println("Are they equal? " + exp.equals(one));
-
-    cmp = 0;
-		Pair two = mmB(0, a.length - 1, a);
-
-    System.out.println("Found min/max mmB in " + cmp + " compares. Should have gotten: " + optimal);
-    System.out.println("Are they equal? " + exp.equals(two));
-		
-		System.out.println("");
-	}
+    return;
+  }
 }
 	
